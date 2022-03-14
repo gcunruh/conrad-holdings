fb528902b79702c22caec2669f1b9250db759138
gcunruh <49214225+gcunruh@users.noreply.github.com>
2022-03-14 14:50:29 -0500

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: 'jit', // ⚠ Make sure to have this
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+      extend: {}
+    },
+    plugins: [],
+    darkMode: 'media', // or 'media' or 'class'
+  }
