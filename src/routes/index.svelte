49ebdf9683fe4ffa41095b003b5f94f873e368ec
gcunruh <49214225+gcunruh@users.noreply.github.com>
2022-03-15 01:24:38 -0500

    apostrophy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,7 +22,7 @@
 <section class="mt-9 md:mt-0 pb-12">
 	<h1 class="font-semibold text-xl tracking-wide">About</h1>
 	<div>
-		HQ'ed in Austin, Texas; Conrad Holdings is a collection of Internet Businesses. You can learn more about Conrad Holdings co-owner at this <a class="underline hover:text-slate-600" href="https://gabrielunruh.com" target="_blank">link</a>.
+		HQ'ed in Austin, Texas; Conrad Holdings is a collection of Internet Businesses. You can learn more about Conrad Holdings' co-owner at this <a class="underline hover:text-slate-600" href="https://gabrielunruh.com" target="_blank">link</a>.
 	</div>
 </section>
 </div>
