2e42a9fdc09e249910c12288a29d7a024c76920d
gcunruh <49214225+gcunruh@users.noreply.github.com>
2022-03-16 11:15:34 -0500

    featured portco

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,18 @@
 
 <div class="mx-10 md:mx-14 lg:mx-24">
 <section>
-	<div class="my-2 md:my-12 grid grid-cols-1 md:grid-cols-4 gap-4 lg:gap-24">
+	<a href="https://mushroom.party">
+		<div class="hover:bg-white justify-center flex flex-col border-2 border-black rounded-md px-4 diagstripes py-1">
+			<div>
+				<h2 class="font-normal text-center">
+					<span class="font-semibold">!Featured Portco¡</span> Mushroom Party is a decentralized polling platform built on Solana. Launching on mainnet Mar 20!
+				</h2>
+			</div>
+		</div>
+		</a>
+</section>
+<section>
+	<div class="my-10 md:my-12 grid grid-cols-1 md:grid-cols-4 gap-4 lg:gap-24">
 		<Card header="Divination" description="Product studio dedicated to exceptional craftsmanship in technology" bg="circles" href="https://divination.dev" />
 		<Card header="Research" description="Cyber advisory and research" bg="graphpaper" />
 		<Card header="Crypto Fund" description="Opportunistic crypto assets fund" bg="banknote" />
