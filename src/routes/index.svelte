acbe94f6fac1ce9d65eb5754f9d85645226622f5
Gabriel <g@gabrielunruh.com>
2022-12-07 18:40:03 +0000

    small updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,12 +7,12 @@
   <title>Home</title>
 </svelte:head>
 
-<div class="mx-10 md:mx-14 lg:mx-28">
+<div class="mx-10 md:mx-14 lg:mx-28 xl:mx-48">
   <section>
     <div class="my-4 md:my-10 grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-24">
       <Card
         header="Divination"
-        description="Full-service product studio making magic in the digital age"
+        description="Full-service product consultancy making magic in the digital age"
         bg="circles"
         href="https://divination.dev" />
       <Card
